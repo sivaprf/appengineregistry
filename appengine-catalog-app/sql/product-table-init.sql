b6243f9de20838ad487fb5e91c12267de81e2ffa
Hamish Ian Reid <hreid@hreid-macbookpro.roam.corp.google.com>
2017-09-05 16:48:03 -0700

    Fixed image URLs to point to new static CDN.

--- appengine-catalog-app/sql/product-table-init.sql
+++ appengine-catalog-app/sql/product-table-init.sql
@@ -1,47 +1,47 @@
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('ZZ-0000-Z', 'Test Entry 01', '100% cotton jersey fabric makes this Google t-shirt perfect for the Google hero in your life. Made in USA.',
 	'46.99', 'Women\'s Apparel', 'T-Shirts', '100% Cotton\n30 Singles jersey\nCrew neck\nMade in USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0278-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0278.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0278-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0278.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('AB-0983-Z', 'Google Women\'s Short Sleeve Hero Tee Black', '100% cotton jersey fabric makes this Google t-shirt perfect for the Google hero in your life. Made in USA.',
 	'16.99', 'Women\'s Apparel', 'T-Shirts', '100% Cotton\n30 Singles jersey\nCrew neck\nMade in USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0278-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0278.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0278-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0278.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('AE-1283-M', 'Google Women\'s Long Sleeve Blended Cardigan Charcoal', 'Made in the USA quality and a soft-feeling triple blended material make this cardigan a winner in cool weather or cold offices.',
 	'20.99', 'Women\'s Apparel', 'Outerwear', '38% cotton/50% polyester/12% rayon\nMade in USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0308-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0308.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0308-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0308.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('EE-9122-E', 'Google Men\'s 100% Cotton Short Sleeve Hero Tee White', 'Be a Google Hero. 100% cotton jersey fabric sets this Google t-shirt above the crowd. Features the Google logo across the chest.',
 	'16.99', 'Men\'s Apparel', 'T-Shirts', '100% Cotton\n30 Singles Jersey\nMade in the USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0104-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0104.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0104-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0104.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('EF-4454-A', 'Android Men\'s Engineer Short Sleeve Tee Charcoal', '100% cotton fabric and the Android robot logo combines comfort and style to this t-shirt.',
 	'19.99', 'Men\'s Apparel', 'T-Shirts', '100% Cotton\n30 Singles Jersey\nMade in the USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEAXXX0808-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEAXXX0808.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEAXXX0808-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEAXXX0808.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('ZZ-0881-A', 'Google Men\'s Vintage Badge Tee Black', 'This Google t-shirt is soft, comfortable, and lets you show your Google affinity with a classic style. 50/50 cotton-polyester blend.',
 	'9.49', 'Men\'s Apparel', 'T-Shirts', '4.4 ounce, 50/50 cotton/poly\nTear away label\nRib-bound neck',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0338-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0338.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0338-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0338.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('AL-5565-Z', 'Google Tri-blend Hoodie Grey', 'Tri-blend jersey fabric lends this Google hoodie a distinct appearance. Made in the USA. Decoration on the left chest.',
 	'39.99', 'Men\'s Apparel', 'Outerwear', '4.5 oz. jersey knit\n50% polyester, 37.5%cotton, 12.5% rayon\n65% polyester/35% cotton sleeves\nMade in the USA',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0313-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0313.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0313-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0313.jpg');
 
 INSERT INTO `gclouddemo_catalog`.`products` (`sku`, `summary`, `description`, `price`, `category`, `subcategory`, `details`, `image`, `thumb`)
 VALUES ('BE-0921-T', 'Google Women\'s Long Sleeve Tee Lavender', 'Casual, comfortable, and essentially Google. This Google t-shirt transcends the typical with its arresting hemline and 3/4 sleeves.',
 	'12.00', 'Women\'s Apparel', 'T-Shirts', '50% cotton, 50% polyester\nCurved shirttail hem\nGoogle logo decoration',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0363-th1.jpg',
-	'https://storage.googleapis.com/ecom-dev-catalog-images/GGOEGAAX0363.jpg');
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0363-th1.jpg',
+	'https://static.catalog.app.gclouddemo.com/images/GGOEGAAX0363.jpg');
